4da5c4b4213439939280b1297648896a639a87c1
Cambier Gaetan <gaetan@cambier.org>
2014-12-04 17:58:32 +0100

    SquidGuard SPK: Bug Correction

--- spk/squidguard/src/installer.sh
+++ spk/squidguard/src/installer.sh
@@ -14,9 +14,10 @@ ETC_DIR="${INSTALL_DIR}/etc/"
 WWW_DIR="/var/packages/${PACKAGE}/target/share/www/squidguardmgr"
 WEBMAN_DIR="/usr/syno/synoman/webman/3rdparty"
 SQUID_WRAPPER="${WWW_DIR}/squid_wrapper"
-
+SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
 SERVICETOOL="/usr/syno/bin/servicetool"
 FWPORTS="/var/packages/${PACKAGE}/scripts/${PACKAGE}.sc"
+TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
 
 preinst ()
 {
@@ -37,12 +38,13 @@ postinst ()
     # Patch template files
     hostname=`hostname`
     sed "s/==HOSTNAME==/$hostname/g" ${ETC_DIR}/squidguard.conf.tpl > ${ETC_DIR}/squidguard.conf
-    
+    sed "s/==HOSTNAME==/$hostname/g" ${ETC_DIR}/squidclamav.conf.tpl > ${ETC_DIR}/squidclamav.conf    
     # Correct the files ownership
     chown -R ${RUNAS}:users ${SYNOPKG_PKGDEST}
-    chown 0:0 ${SQUID_WRAPPER}
-	chmod +s ${SQUID_WRAPPER}
-	
+
+    # need when new DB files are created via web interface (avoid permission error because squidGuard is launch with squid user)
+    chmod u+s ${INSTALL_DIR}/bin/squidGuard
+
     # Init squid cache directory
     su - ${RUNAS} -c "${SQUID} -z -f ${CFG_FILE}"
 
@@ -53,15 +55,20 @@ postinst ()
     # For squidclamav redirect
     ln -sf ${INSTALL_DIR}/libexec/squidclamav/clwarn.cgi ${WEBMAN_DIR}/${PACKAGE}/clwarn.cgi
 
-      
-    # Init crontab : update squidguard DB each day at 1 a.m
+    # Init crontab :
+    #    update squidguard DB each day at 1 a.m
+    #    logs rotate every sunday at 0:15 a.a
     grep ${PACKAGE} /etc/crontab
     if [ $? -eq 1 ]; then
-        echo "0 1       *       *       *       root    ${INSTALL_DIR}/bin/update_db.sh > ${INSTALL_DIR}/var/logs/update_db.log" >> /etc/crontab
-        /usr/syno/etc/rc.d/S04crond.sh stop
+        echo "0	1	*	*	*	root	${INSTALL_DIR}/bin/update_db.sh > ${INSTALL_DIR}/var/logs/update_db.log" >> /etc/crontab
+        echo "15	0	*	*	0	root	${INSTALL_DIR}/bin/logrotate.sh > ${INSTALL_DIR}/bin/logrotate.log" >> /etc/crontab
+        /usr/syno/etc/rc.d/S04crond.sh stop >> /dev/null
         sleep 1
-        /usr/syno/etc/rc.d/S04crond.sh start
+        /usr/syno/etc/rc.d/S04crond.sh start >> /dev/null
     fi
+
+    # launch first the update
+    ${INSTALL_DIR}/bin/update_db.sh > ${INSTALL_DIR}/var/logs/update_db.log 2>&1
 
     # Add firewall config
     ${SERVICETOOL} --install-configure-file --package ${FWPORTS} >> /dev/null
@@ -71,7 +78,6 @@ postinst ()
 
 preuninst ()
 {
-
     # Remove firewall config
     if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" ]; then
         ${SERVICETOOL} --remove-configure-file --package ${PACKAGE}.sc >> /dev/null
@@ -86,6 +92,9 @@ postuninst ()
     rm -f ${INSTALL_DIR}
     rm -Rf ${WEBMAN_DIR}/${PACKAGE}
     sed "/${PACKAGE}/d" /etc/crontab
+    /usr/syno/etc/rc.d/S04crond.sh stop >> /dev/null
+    sleep 1
+    /usr/syno/etc/rc.d/S04crond.sh start >> /dev/null
     deluser ${RUNAS}
     
     exit 0
@@ -105,9 +114,13 @@ postupgrade ()
 {
     # Restore some stuff
     rm -fr ${INSTALL_DIR}/etc
+    mv ${INSTALL_DIR}/etc/squidclamav.conf ${TMP_DIR}/${PACKAGE}/etc/squidclamav.conf.new
+    mv ${INSTALL_DIR}/etc/squidguard.conf ${TMP_DIR}/${PACKAGE}/etc/squidguard.conf.new
+    mv ${INSTALL_DIR}/etc/squid.conf ${TMP_DIR}/${PACKAGE}/etc/squid.conf.new
     mv ${TMP_DIR}/${PACKAGE}/etc ${INSTALL_DIR}/
     rm -fr ${TMP_DIR}/${PACKAGE}
-
+    ${SSS} stop > /dev/null
+    ${SSS} start > /dev/null
     exit 0
 }
 
